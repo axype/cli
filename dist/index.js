e6967f2af355e8fcba1b5ba4e08d26f6f999a17d
Darkceius <74603733+darkceius@users.noreply.github.com>
2025-06-17 22:21:55 +0300

    add init cmd

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,212 @@
 #!/usr/bin/env node
 
+// axype cli
+// @darkceius
+const yargs = require("yargs");
+const { prompt } = require("enquirer");
+const { hideBin } = require("yargs/helpers");
+const colors = require("ansi-colors");
+const symbols = colors.symbols;
+const process = require("node:process");
+const childProcess = require("node:child_process");
+const fs = require("node:fs");
+const path = require("node:path");
+
+const hasCLI = function (name) {
+	const response = childProcess.spawnSync(name, ["-v"]);
+	return response.error == undefined;
+};
+
+const init = async function () {
+	if (!hasCLI("git")) {
+		console.log(
+			`${colors.magenta(symbols.cross)} Git is required to use this command!`
+		);
+		process.exit(1);
+	}
+
+	if (!hasCLI("rokit")) {
+		console.log(
+			`${colors.magenta(
+				symbols.cross
+			)} Rokit is not installed on this system!\n${colors.cyan(
+				symbols.pointerSmall
+			)} Download it here: https://github.com/rojo-rbx/rokit/releases/latest`
+		);
+		process.exit(1);
+	}
+
+	const data = await prompt([
+		{
+			type: "input",
+			name: "name",
+			message: "What will the project be named?",
+		},
+		{
+			type: "confirm",
+			name: "useGit",
+			message: "Initialize git repository?",
+			initial: true,
+		},
+		{
+			type: "confirm",
+			name: "useClient",
+			message: "Add client-side support? (src/client)",
+			initial: true,
+		},
+	]);
+
+	const projectName = data.name.replace(/[^a-zA-Z0-9]/g, "").trim();
+	const useClient = data.useClient;
+
+	let data2;
+	if (useClient) {
+		data2 = await prompt([
+			{
+				type: "confirm",
+				name: "useClientLuau",
+				message: "Use Luau syntax for client?",
+				initial: false,
+			},
+		]);
+	}
+
+	const cwd = process.cwd();
+	const targetPath = path.join(cwd, projectName);
+
+	// confirmation prompt
+	const confirmation = await prompt([
+		{
+			type: "confirm",
+			name: "proceed",
+			message: `Are you sure you want to initialize repository at ${colors.cyan(
+				targetPath
+			)}`,
+			initial: true,
+		},
+	]);
+
+	if (!confirmation.proceed) {
+		console.log(`${colors.magenta(symbols.cross)} Cancelled!`);
+		return;
+	}
+
+	// preventing overwriting
+	if (fs.existsSync(targetPath)) {
+		console.log(
+			`${colors.magenta(
+				symbols.cross
+			)} Couldn't init because a file with the same target name already exists in this directory!`
+		);
+		process.exit(1);
+	}
+
+	console.log(
+		`${colors.cyan(symbols.info)} Creating files! Might take some seconds...`
+	);
+
+	// cloning repo template
+	{
+		console.log(`${colors.cyan(symbols.info)} Cloning template repository...`);
+
+		const gitURL = useClient
+			? "https://github.com/axype/paste-template.git"
+			: "https://github.com/axype/paste-template-noclient.git";
+
+		try {
+			const spawned = childProcess.spawnSync(
+				"git",
+				["clone", gitURL, projectName],
+				{
+					cwd,
+				}
+			);
+			if (spawned.error) console.error(spawned.error);
+			if (spawned.status !== 0) console.error(spawned.stderr.toString());
+		} catch (err) {
+			console.log(
+				`${colors.magenta(
+					symbols.cross
+				)} Something went wrong while cloning the template repository!`
+			);
+			console.error(err);
+			process.exit(1);
+		}
+
+		console.log(`${colors.green(symbols.check)} Successfully cloned!`);
+	}
+
+	console.log(`${colors.cyan(symbols.info)} Finishing up...`);
+
+	// changing json files
+	{
+		const defaultProject = path.join(targetPath, "default.project.json");
+		const contents = JSON.parse(fs.readFileSync(defaultProject, "utf-8"));
+		contents["name"] = projectName;
+		fs.writeFileSync(defaultProject, JSON.stringify(contents));
+	}
+
+	// use client luau syntax
+	if (data2 && data2.useClientLuau) {
+		fs.renameSync(
+			path.join(targetPath, "src", "client", "init.lua"),
+			path.join(targetPath, "src", "client", "init.luau")
+		);
+	}
+
+	// initializing rokit
+	childProcess.spawnSync("rokit", ["install"], { cwd: targetPath });
+	console.log(`${colors.green(symbols.check)} Initialized Rokit!`);
+
+	// .git related stuff
+	{
+		if (fs.existsSync(path.join(targetPath, ".git"))) {
+			fs.rmSync(path.join(targetPath, ".git"), {
+				force: true,
+				recursive: true,
+			});
+		}
+
+		const useGit = data.useGit;
+		if (useGit) {
+			childProcess.spawnSync("git", ["init"], { cwd: targetPath });
+			childProcess.spawnSync("git", ["add", "."], {
+				cwd: targetPath,
+			});
+			childProcess.spawnSync("git", ["commit", "-m", "Initial commit"], {
+				cwd: targetPath,
+			});
+		}
+	}
+
+	console.log(`${colors.green(symbols.check)} Successfully created project!`);
+
+	// open in vscode prompt
+	{
+		const response = await prompt([
+			{
+				type: "confirm",
+				name: "openCode",
+				message: "Open project in VS Code?",
+				initial: false,
+			},
+		]);
+
+		if (response.openCode) {
+			childProcess.spawnSync("code", [".", "./src/server/init.luau"], {
+				cwd: targetPath,
+				shell: true,
+			});
+		}
+	}
+};
+
+yargs(hideBin(process.argv))
+	.command("init", "initializes a template Axype git project", () => {}, init)
+
+	.alias("h", "help")
+	.alias("i", "init")
+
+	.demandCommand(1, "Command is required")
+
+	.parse();
